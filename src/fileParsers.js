a785c1fd721e8df32ebfc8bca7f10c37b2464c63
Michael Hall <hall@michaelhall.se>
2021-03-31 18:06:53 +0200

    Start with functionality for parsing clover coverage file

--- src/fileParsers.js
+++ src/fileParsers.js
@@ -1,16 +1,86 @@
 const fs = require('fs');
+const xml2js = require('xml2js');
 
+/**
+ * Clover file parser class.
+ */
 class CloverFileParser {
+    /**
+     * Parses a clove file.
+     *
+     * @param {string} filename
+     *
+     * @return {{CodeCoveragePercentage: number}}
+     */
     parseFile(filename) {
         if (!fs.existsSync(filename)) {
-            throw new Error("Could not find file \"" + filename + "\"");
+            throw new Error('Could not find file "' + filename + '"');
         }
 
-        let codeCoveragePercentage = 50; // fixme: Actually calculate this value.
+        try {
+            const parsedXml = this.parseXmlFile(filename);
 
-        return {
-            CodeCoveragePercentage: codeCoveragePercentage,
-        };
+            const coverageNode = this.getParsedXmlNode(parsedXml, 'coverage');
+            const projectNode = this.getParsedXmlNode(coverageNode, 'project');
+            const metricsNode = this.getParsedXmlNode(projectNode, 'metrics');
+
+            let codeCoveragePercentage = 100; // fixme: Actually calculate this value.
+
+            return {
+                CodeCoveragePercentage: codeCoveragePercentage,
+            };
+        } catch (error) {
+            throw new Error('File "' + filename + '" is not a valid clover file: ' + error.message);
+        }
+    }
+
+    /**
+     * Parses an XML file into an object.
+     *
+     * @private
+     *
+     * @param {string} filename
+     *
+     * @return {Object}
+     */
+    parseXmlFile(filename) {
+        const xmlFileContent = fs.readFileSync(filename, 'utf-8');
+
+        let parseResult = {};
+        let parseError = null;
+
+        const xmlParser = new xml2js.Parser({'explicitArray': false});
+        xmlParser.parseString(xmlFileContent, function (error, result) {
+            if (error === null) {
+                parseResult = result;
+            } else {
+                parseError = error;
+            }
+        })
+
+        if (parseError !== null) {
+            throw new Error('XML is invalid: ' + parseError.message);
+        }
+
+        return parseResult;
+    }
+
+    /**
+     * Returns a node from parsed XML.
+     *
+     * @private
+     *
+     * @param {Object} content
+     * @param {string} nodeName
+     *
+     * @return {Object}
+     */
+    getParsedXmlNode(content, nodeName) {
+        if (!(nodeName in content)) {
+            throw new Error('Node "<' + nodeName + '>" is missing');
+        }
+
+        return content[nodeName];
     }
 }
 
--- test/fileParsers.test.js
+++ test/fileParsers.test.js
@@ -8,9 +8,29 @@ test('Read clover code coverage from non-existing file', () => {
     }).toThrow('Could not find file "./test/foo.xml"');
 });
 
-test('Read clover code coverage from valid file', () => {
+test('Read clover code coverage from an invalid file #1', () => {
     const cloverFileParser = new fileParsers.CloverFileParser();
-    const result = cloverFileParser.parseFile('./test/test.xml');
 
-    expect(result.CodeCoveragePercentage).toBe(50);
+    expect(() => {
+        cloverFileParser.parseFile('./test/clover-invalid-1.txt');
+    }).toThrow("File \"./test/clover-invalid-1.txt\" is not a valid clover file: XML is invalid: Non-whitespace before first tag.\nLine: 0\nColumn: 1\nChar: H");
+});
+
+test('Read clover code coverage from an invalid file #2', () => {
+    const cloverFileParser = new fileParsers.CloverFileParser();
+
+    expect(() => {
+        cloverFileParser.parseFile('./test/clover-invalid-2.xml');
+    }).toThrow('File "./test/clover-invalid-2.xml" is not a valid clover file: Node "<coverage>" is missing');
+});
+
+// fixme: Invalid with invalid parameter type
+// fixme: File with 0% coverage
+// fixme: File with partial coverage
+
+test('Read clover code coverage from a file with full coverage', () => {
+    const cloverFileParser = new fileParsers.CloverFileParser();
+    const result = cloverFileParser.parseFile('./test/clover-full-coverage.xml');
+
+    expect(result.CodeCoveragePercentage).toBe(100);
 });
